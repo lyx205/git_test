98af10724bb3024dc8c9563dca97d60dfed243ce
Yixuan <long_yixuan@hrbeu.edu.cn>
2022-01-02 19:40:37 +0800

    SSH commit

--- main.cpp
+++ main.cpp
@@ -7,5 +7,6 @@ int main() {
     std::cout << "Hello, World hot-fix!" << std::endl;
     std::cout << "master test!" << std::endl;
     std::cout << "hot-fix test!" << std::endl;
+    std::cout << "SSH test!" << std::endl;
     return 0;
 }
