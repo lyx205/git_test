9724a8f1513c76ef5ad2a0c24d2eff79207f3e64
Yixuan <long_yixuan@hrbeu.edu.cn>
2022-01-02 17:40:44 +0800

    hot-fix commit1

--- main.cpp
+++ main.cpp
@@ -4,5 +4,6 @@ int main() {
     std::cout << "Hello, World!" << std::endl;
     std::cout << "Hello, World2!" << std::endl;
     std::cout << "Hello, World3!" << std::endl;
+    std::cout << "Hello, World hot-fix!" << std::endl;
     return 0;
 }
